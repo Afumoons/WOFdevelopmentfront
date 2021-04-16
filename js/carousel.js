47427dc4ac3af5d4a88e3b47c5adfbcb4dd633ed
Afumoons <afumoons@gmail.com>
2021-04-16 16:11:50 +0700

    Proses Detail

--- js/carousel.js
+++ js/carousel.js
@@ -176,6 +176,7 @@ var swiperreview = new Swiper(".swiper-container.review-carousel", {
   effect: "slide",
   speed: 1000,
   // 'slide' | 'fade' | 'cube' | 'coverflow' | 'flip'
+  slidesPerView: 2,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -190,7 +191,7 @@ var swiperreview = new Swiper(".swiper-container.review-carousel", {
   },
   breakpoints: {
     640: {
-      slidesPerView: 1,
+      slidesPerView: 2,
     },
     768: {
       slidesPerView: 2,
